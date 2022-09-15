eb29bd27dc4ba8e4332ffcab3191505a5819dcc6
zhenghaiyan <1963764437@qq.com>
2022-09-15 09:47:59 +0800

    添加模型

--- src/views/main.vue
+++ src/views/main.vue
@@ -94,8 +94,7 @@ export default {
           qtum3d.layer.createLayer(
             {
               type: "3dtiles",
-              // url: "http://124.71.153.0:31080/mmodel/model/Scene1/Production_2.json",
-              // url:'http://218.94.58.222:32080/zl/Scene/Scene1/Production_2.json',
+              url:'http://117.174.152.90:22286/upload/Scene/Production.json',
               offset: { z: -12 },
               visible: !0,
               flyTo: true,
